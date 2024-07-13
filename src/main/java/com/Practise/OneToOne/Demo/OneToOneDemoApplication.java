f95217a6fc5ba277b18cfe28eef96ea6bcf70575
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-13 22:02:42 +0530

    Deployment

---
+++ src/main/java/com/Practise/OneToOne/Demo/OneToOneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Practise.OneToOne.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practise/OneToOne/Demo/OneToOneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practise.OneToOne.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
